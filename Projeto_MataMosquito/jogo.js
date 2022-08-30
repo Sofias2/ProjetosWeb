04ada2199a4ee97e09d42ddf6a8da62013530efe
Sofias2 <sofiacosta5@outlook.com'git config --global user.name Sofiagit config --global user.email sofiacosta5@outlook.comgit config --global user.name "Sofiagit config --global user.email "sofiacosta5@outlook.com>
2022-08-30 14:34:17 -0300

    Nova Atualização no Projeto - Em andamento

--- Projeto_MataMosquito/jogo.js
+++ Projeto_MataMosquito/jogo.js
@@ -1,11 +1,87 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
 	largura = window.innerWidth
-	console.log( altura , altura)
+	
 }
 	
 ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica() {
+
+if (document.getElementById('mosca')) {	
+	document.getElementById('mosca').remove()
+
+	if (vidas > 3) {
+		alert('Game Over')
+	}
+	document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+	vidas++
+
+
+}
+
+var posicaoX = Math.floor(Math.random() * largura) - 90
+var posicaoY = Math.floor(Math.random() * altura) - 90
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+var mosca = document.createElement('img')
+mosca.src = 'imagens/mosca.png'
+mosca.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+mosca.style.left = posicaoX + 'px'
+mosca.style.top = posicaoY + 'px'
+mosca.style.position = 'absolute'
+mosca.id = 'mosca'
+mosca.onclick = function () {
+	this.remove()
+}
+
+
+
+document.body.appendChild(mosca)
+
+
+	
+}
+
+
+function tamanhoAleatorio() {
+	var classe =  Math.floor(Math.random() * 3)
+
+
+	switch(classe){
+		case 0:
+			return 'mosquito1'
+
+		case 1:
+			return 'mosquito2'
+
+		case 2:
+			return "mosquito3"
+		}
+
+}
+
+function ladoAleatorio() {
+	var classe =  Math.floor(Math.random() * 2)
+
+
+	switch(classe){
+		case 0:
+			return 'ladoA'
+
+		case 1:
+			return 'ladoB'
+
+		
+}
+}
+
+
+
 
