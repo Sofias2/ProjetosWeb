9f91c8cb49bf6b2a8837f671b8a9cb28177ab20e
Sofias2 <sofiacosta5@outlook.com'git config --global user.name Sofiagit config --global user.email sofiacosta5@outlook.comgit config --global user.name "Sofiagit config --global user.email "sofiacosta5@outlook.com>
2022-09-16 14:02:54 -0300

    Novo Projeto - Atualização

--- Projeto_AppOrcamentoPessoal/app.js
+++ Projeto_AppOrcamentoPessoal/app.js
@@ -1,5 +1,5 @@
 class Despesa {
-	constructor(ano, mes, dia, tipo, descricao, valor){
+	constructor(ano, mes, dia, tipo, descricao, valor) {
 		this.ano = ano
 		this.mes = mes
 		this.dia = dia
@@ -7,34 +7,64 @@ class Despesa {
 		this.descricao = descricao
 		this.valor = valor
 	}
+
+	validarDados() {
+		for(let i in this) {
+			if(this[i] == undefined || this[i] == '' || this[i] == null) {
+				return false
+			}
+		}
+		return true
+	}
 }
 
-class BD{
+class Bd {
 
-	constructor(){
+	constructor() {
 		let id = localStorage.getItem('id')
 
-		if (id === null) {
+		if(id === null) {
 			localStorage.setItem('id', 0)
 		}
 	}
 
-	getProximoID(){
+	getProximoId() {
 		let proximoId = localStorage.getItem('id')
-		return parseInt(proximoId)+1
+		return parseInt(proximoId) + 1
 	}
 
-	gravar(d){
-	
-	let id = this.getProximoID()
+	gravar(d) {
+		let id = this.getProximoId()
 
-	localStorage.setItem(id, JSON.stringify(d))
+		localStorage.setItem(id, JSON.stringify(d))
 
-	localStorage.setItem('id', id)
+		localStorage.setItem('id', id)
+	}
+
+	recuperaRegistros() {
+
+		//array de despesas
+		let despesas = Array()
+
+		let id = localStorage.getItem('id')
+
+		for(let i = 1; i <= id; i++) {
+
+			let despesa = JSON.parse(localStorage.getItem(i))
+
+			if(despesa === null) {
+				continue
+			}
+
+			despesas.push(despesa)
+		}
+
+		return despesas
 	}
 }
 
-let bd = new BD()
+let bd = new Bd()
+
 
 function cadastrarDespesa() {
 
@@ -45,13 +75,84 @@ function cadastrarDespesa() {
 	let descricao = document.getElementById('descricao')
 	let valor = document.getElementById('valor')
 
+	let despesa = new Despesa(
+		ano.value, 
+		mes.value, 
+		dia.value, 
+		tipo.value, 
+		descricao.value,
+		valor.value
+	)
 
-	let despesa = new Despesa( ano.value, 
-		mes.value, dia.value, tipo.value, descricao.value, valor.value)
 
-	
+	if(despesa.validarDados()) {
+		bd.gravar(despesa)
 
-	bd.gravar(despesa)
+		document.getElementById('modalTitulo').innerHTML = 'Registro inserido com sucesso'
+		document.getElementById('modaltituloDiv').className = 'modal-header text-success'
+		document.getElementById('modalConteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+		document.getElementById('modalBtn').innerHTML = 'Voltar'
+		document.getElementById('modalBtn').className = 'btn btn-success'
+
+		$('#modalRegistraDespesa').modal('show') 
+		
+		ano.value = ''
+		mes.value = ''
+		dia.value = ''
+		tipo.value = ''
+		descricao.value = ''
+		valor.value = ''
+
+	} else {
+		
+		document.getElementById('modalTitulo').innerHTML = 'Erro na inclusão do registro'
+		document.getElementById('modaltituloDiv').className = 'modal-header text-danger'
+		document.getElementById('modalConteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
+		document.getElementById('modalBtn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modalBtn').className = 'btn btn-danger'
+
+		$('#modalRegistraDespesa').modal('show') 
+	}
+}
+
+
+
+
+function carregaListaDespesas(){
+	let despesas = Array()
+	despesas = bd.recuperaRegistros()
+
+	let listaDespesas = document.getElementById('listaDespesas')
+
+	despesas.forEach(function (d) {
+
+		let linha = listaDespesas.insertRow()
+
+		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano} `
+		
+
+		switch(d.tipo){
+			case '1' : d.tipo = 'Alimentação'
+				break
+			case '2' : d.tipo = 'Educação'
+				break
+			case '3' : d.tipo = 'Lazer'
+				break
+			case '4' : d.tipo = 'Saúde'
+				break	
+			case '5' : d.tipo = 'Transporte'
+				break	
+
+		}
+
+		linha.insertCell(1).innerHTML = d.tipo
+
+		linha.insertCell(2).innerHTML = d.descricao
+		linha.insertCell(3).innerHTML = d.valor
+		
+
+		
+	})
 
 }
 
